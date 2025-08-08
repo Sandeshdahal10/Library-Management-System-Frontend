89afba8a93afd538200d5d267545e5ef01023c46
Sandesh Dahal <sandeshdahal3547@gmail.com>
2025-08-08 08:25:58 +0545

    sidebar design completed

--- src/Layout/Sidebar.jsx
+++ src/Layout/Sidebar.jsx
@@ -1,46 +1,54 @@
-// import { FaBookOpen } from "react-icons/fa";
 import { ImBooks } from "react-icons/im";
 import { MdHistory } from "react-icons/md";
 import { CiCirclePlus } from "react-icons/ci";
 import { MdOutlineQueryStats } from "react-icons/md";
-import { Link,useLocation } from "react-router-dom";
-export default function Sidebar(){
-  return(
+import { Link, useLocation } from "react-router-dom";
+export default function Sidebar() {
+  return (
     <>
-    <div className="w-64 border-r shadow-md p-4 min-h-screen bg-primary">
-      <nav className="flex flex-col gap-4 text-gray-400">
-        <SidebarItems>
-          icon={<ImBooks />}
-          label="Manage books"
-        </SidebarItems>
-        <SidebarItems>
-          icon={<MdHistory />}
-          label="Borrow records"
-        </SidebarItems>
-        <SidebarItems>
-          icon={<CiCirclePlus />}
-          label="Add new book"
-        </SidebarItems>
-        <SidebarItems>
-          icon={<MdOutlineQueryStats />}
-          label="Stats"
-        </SidebarItems>
-       
-      </nav>
-    </div>
+      <div className="w-64 border-r shadow-md p-4 min-h-screen bg-primary">
+        <nav className="flex flex-col gap-4 text-white">
+          <SidebarItems
+            icon={<ImBooks />}
+            label="Manage books"
+            to="/librarian/managebooks"
+          />
+
+          <SidebarItems
+            icon={<MdHistory />}
+            label="Borrow records"
+            to="/librarian/borrow-records"
+          />
+
+          <SidebarItems
+            icon={<CiCirclePlus />}
+            label="Add new book"
+            to="/librarian/add-book"
+          />
+
+          <SidebarItems
+            icon={<MdOutlineQueryStats />}
+            label="Stats"
+            to="/librarian/stats"
+          />
+        </nav>
+      </div>
     </>
-  )
+  );
 }
 
 const SidebarItems = ({ icon, label, to }) => {
-    const location = useLocation();
-    const active = location.pathname === to;
-    const baseclass = "flex items-center  gap-2 py-2 px-2  text-black";
-    const activeClass = active ? "bg-black text-primary rounded" : "bg-none";
-    return (
-        <Link to={to} className="cursor-pointer text-2xl">
-            <span className="text-lg">{icon}</span>
-            <span className="text-sm">{label}</span>
-        </Link>
-    );
+  const location = useLocation();
+  const active = location.pathname === to;
+  const baseclass = "flex items-center  gap-2 py-4 px-6  ";
+  const activeClass = active ? " text-white rounded" : "text-white";
+  return (
+    <Link
+      to={to}
+      className={`cursor-pointer text-2xl  ${activeClass} ${baseclass}`}
+    >
+      <span className="text-lg">{icon}</span>
+      <span className="text-sm">{label}</span>
+    </Link>
+  );
 };
