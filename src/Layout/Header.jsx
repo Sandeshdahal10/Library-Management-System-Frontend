f4edcac72c536fed74c9ae420769f5c9ddd5faf2
Sandesh Dahal <sandeshdahal3547@gmail.com>
2025-08-08 08:40:56 +0545

    Navbar design completed

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,13 +1,26 @@
-export  function Header(){
-  return(
+import { FaBookOpen } from "react-icons/fa";
+import { FaSearch, FaBell, FaUser } from "react-icons/fa";
+import LogoutButton from "../utils/Logout";
+
+export function Header() {
+  return (
     <>
-    <nav>
-      <div>
-        
+      <div className="flex  justify-between bg-primary">
+        <div className="flex items-center p-2 gap-2.5">
+          <FaBookOpen className="cursor-pointer text-4xl text-blue-500" />
+          <h1 className="text-3xl text-blue-500 font-bold">
+            Book Nest Library
+          </h1>
+        </div>
+        <div className="flex items-center justify-between text-5xl gap-6 mr-4">
+          <FaSearch className="cursor-pointer text-2xl text-blue-500" />
+          <FaBell className="cursor-pointer text-2xl text-blue-500" />
+          <FaUser className="cursor-pointer text-2xl text-blue-500" />
+          <LogoutButton />
+        </div>
       </div>
-    </nav>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
